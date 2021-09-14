b51ae0b157ced07a018604d6beb764b288d5c372
Steven Weißheimer <mail@stevenweissheimer.com>
2021-09-14 11:34:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -166,3 +166,16 @@ if (require.main !== module) {
 	new JonnySpeisekarteWeb();
 }
 
+const express = require('express')
+const app = express()
+const port = 4000
+// const test =  getState('0_userdata.0.tagesgericht.gericht1.title').val;
+const test = "Test"
+
+app.get('/', (req, res) => {
+  res.send('Hello World github?' + test + '<br>Test')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
